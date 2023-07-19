7d2b929b13a91b89e72db1a78debe1e6280d375a
Kaio Soledade <soledadekaio@gmail.com>
2023-07-19 18:30:58 -0300

    JavaScript Backend

---
+++ JS-Backend-(Aproximação de algarismos).js
@@ -0,0 +1,11 @@
+//Math.round()
+//Aproximação de um algarismo para o seu valor inteiro mais proximo
+var idade = 3.6
+Math.round(idade)
+console.log(idade)
+
+//Math.ceil
+// Aproximação de um algarismo para o seu valor inteiro mais alto
+var idade1 = 3.6
+Math.ceil(idade1)
+console.log(idade1)
