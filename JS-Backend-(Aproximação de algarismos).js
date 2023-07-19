c7212c85b0e39c6ecc096708ddb825afd60bfc63
Kaio Soledade <soledadekaio@gmail.com>
2023-07-19 18:35:17 -0300

    JavaScript Backend Update

--- JS-Backend-(Aproximação de algarismos).js
+++ JS-Backend-(Aproximação de algarismos).js
@@ -9,3 +9,17 @@ console.log(idade)
 var idade1 = 3.6
 Math.ceil(idade1)
 console.log(idade1)
+
+
+//Math.floor
+//Aproximação de um algarismo para o seu valor inteiro mais baixo
+var idade2 = 3.6
+Math.floor(idade2)
+console.log(idade2)
+
+
+//Math.trunc
+//Desconsidera o valor decimal
+var idade3 = 3.6
+Math.trunc(idade3)
+console.log(idade3)
